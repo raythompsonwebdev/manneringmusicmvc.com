fa7c07ba88f4aa475558cc8d778024d74da77d5a
raythompsonwebdev <ray_thomp@hushmail.com>
2019-01-19 22:38:05 +0000

    test

--- includes/googleanalytics.inc.php
+++ includes/googleanalytics.inc.php
@@ -5,6 +5,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-52392183-2', 'auto');
+  ga('create', '', 'auto');
   ga('send', 'pageview');
 </script>
