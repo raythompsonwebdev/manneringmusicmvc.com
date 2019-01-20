4be52ddff3249ca2b62f6b0d44511c9a612fb608
Raymond Thompson <ray_thomp@hushmail.com>
2019-01-20 20:35:32 +0000

    Update DatabaseConnection.php

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=mannering; charset=utf8', 'root', 'Mu!a8een100');
+$pdo = new PDO('mysql:host=localhost;dbname=; charset=utf8', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
