fd432092fbdfce1fdfd61c450684fec49c2e3fad
raythompsonwebdev <ray_thomp@hushmail.com>
2021-10-08 01:13:57 +0100

    update css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,71 @@
 const sass = require('node-sass');
 
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		babel: {
-			files: {
-				expand: true,
-				src: ['public_html/**/*.js'],
-				ext: '-compiled.js',
-			},
-			options: {
-				sourceMap: true,
-				presets: ['@babel/preset-env'],
-			},
-		},
+    babel: {
+      files: {
+        expand: true,
+        src: ['public_html/**/*.js'],
+        ext: '-compiled.js',
+      },
+      options: {
+        sourceMap: true,
+        presets: ['@babel/preset-env'],
+      },
+    },
 
-		/**
-		 * sass Task
-		 */
-		sass: {
-			options: {
-				implementation: sass,
-				sourceMap: false,
-				sourceMapEmbed: false,
-				sourceMapContents: false,
-			},
-			dev: {
-				options: {
-					style: 'expanded',
-					sourcemap: false,
-				},
+    /**
+     * sass Task
+     */
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: false,
+        sourceMapEmbed: false,
+        sourceMapContents: false,
+      },
+      dev: {
+        options: {
+          style: 'expanded',
+          sourcemap: false,
+        },
 
-				files: {
-					'public_html/style.css': 'public_html/assets/css/style.scss',
-					/*where file goes-----/where file from*/
-				},
-			},
-			dist: {
-				options: {
-					style: 'compressed',
-					sourcemap: false,
-				},
-				files: {
-					'public_html/style-min.css': 'public_html/assets/css/style.scss',
-					/*where file goes-----/where file from*/
-				},
-			},
-		},
+        files: {
+          'public_html/style.css': 'public_html/assets/css/style.scss',
+          /*where file goes-----/where file from*/
+        },
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          sourcemap: false,
+        },
+        files: {
+          'public_html/style-min.css': 'public_html/assets/css/style.scss',
+          /*where file goes-----/where file from*/
+        },
+      },
+    },
 
-		/**
-		 * watch
-		 */
-		watch: {
-			sass: {
-				files: 'public_html/assets/css/*.scss',
-				tasks: ['sass'],
-			},
-		},
-	});
+    /**
+     * watch
+     */
+    watch: {
+      sass: {
+        files: 'public_html/assets/css/*.scss',
+        tasks: ['sass'],
+      },
+    },
+  });
 
-	grunt.loadNpmTasks('grunt-babel');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['babel', 'sass', 'watch']);
+  grunt.registerTask('default', ['babel', 'sass', 'watch']);
 };
 
 /* add bag (!) to wordpress css theme top-title so that it shows on minified file*/
--- templates/artist.html.php
+++ templates/artist.html.php
@@ -15,7 +15,7 @@ $jsonArray = json_encode($array, JSON_UNESCAPED_SLASHES);
 ?>
 
 <script>
-  $(document).ready(function() {
+$(document).ready(function() {
 
     //create seprate playlists for shuffle
     var newPlaylist = <?php echo $jsonArray; ?>;
@@ -28,143 +28,143 @@ $jsonArray = json_encode($array, JSON_UNESCAPED_SLASHES);
     var progressBar = document.querySelector('div.progress div.play_progress');
 
     prevHighlight.addEventListener("mousedown touchstart mousemove touchmove", function(e) {
-      e.preventDefault();
+        e.preventDefault();
     });
 
     progressBar.addEventListener("mousedown", function(e) {
-      mouseDown = true;
+        mouseDown = true;
     });
 
     progressBar.addEventListener("mousemove", function(e) {
-      if (mouseDown == true) {
-        //Set time of song, depending on position of mouse using timeFromOffset function below
-        timeFromOffset(e, this);
-      }
+        if (mouseDown == true) {
+            //Set time of song, depending on position of mouse using timeFromOffset function below
+            timeFromOffset(e, this);
+        }
     });
 
     progressBar.addEventListener("mouseup", function(e) {
-      timeFromOffset(e, this);
+        timeFromOffset(e, this);
     });
 
 
     $("div.audio_volume div.volume").mousedown(function() {
-      mouseDown = true;
+        mouseDown = true;
     });
 
     $("div.audio_volume div.volume").mousemove(function(e) {
-      if (mouseDown == true) {
+        if (mouseDown == true) {
 
-        var percentage = e.offsetX / $(this).width(); //this = div.audio_volume div.volume
+            var percentage = e.offsetX / $(this).width(); //this = div.audio_volume div.volume
 
-        //limits volume range to bewteen 0 and 1
-        if (percentage >= 0 && percentage <= 1) {
-          audioElement.audio.volume = percentage;
+            //limits volume range to bewteen 0 and 1
+            if (percentage >= 0 && percentage <= 1) {
+                audioElement.audio.volume = percentage;
+            }
         }
-      }
     });
 
     $("div.audio_volume div.volume").mouseup(function(e) {
 
-      var percentage = e.offsetX / $(this).width(); //this = div.audio_volume div.volume
+        var percentage = e.offsetX / $(this).width(); //this = div.audio_volume div.volume
 
-      if (percentage >= 0 && percentage <= 1) {
-        audioElement.audio.volume = percentage;
-      }
+        if (percentage >= 0 && percentage <= 1) {
+            audioElement.audio.volume = percentage;
+        }
     });
 
     $(document).mouseup(function() {
-      mouseDown = false;
+        mouseDown = false;
     });
 
-  });
+});
 
-  //us mouse to drag progress bar and change audio position
-  function timeFromOffset(mouse, progressBar) {
+//us mouse to drag progress bar and change audio position
+function timeFromOffset(mouse, progressBar) {
     var percentage = mouse.offsetX / $(progressBar).width() * 100;
     var seconds = audioElement.audio.duration * (percentage / 100);
     audioElement.setTime(seconds); //audio class func
-  }
+}
 
-  //skip to previous song
-  function prevSong() {
+//skip to previous song
+function prevSong() {
     if (audioElement.audio.currentTime >= 3 || currentIndex == 0) {
-      audioElement.setTime(0);
+        audioElement.setTime(0);
     } else {
-      currentIndex = currentIndex - 1;
-      setTrack(currentPlaylist[currentIndex], currentPlaylist, true);
+        currentIndex = currentIndex - 1;
+        setTrack(currentPlaylist[currentIndex], currentPlaylist, true);
     }
-  }
+}
 
-  //skip to next song
-  function nextSong() {
+//skip to next song
+function nextSong() {
     //shuffle
     if (repeat == true) {
-      audioElement.setTime(0);
-      playSong();
-      return;
+        audioElement.setTime(0);
+        playSong();
+        return;
     }
 
     if (currentIndex == currentPlaylist.length - 1) {
-      currentIndex = 0;
+        currentIndex = 0;
     } else {
-      currentIndex++;
+        currentIndex++;
     }
     //shuffle
     var trackToPlay = shuffle ? shufflePlaylist[currentIndex] : currentPlaylist[currentIndex];
     setTrack(trackToPlay, currentPlaylist, true);
-  }
+}
 
-  //repeat button need repeat button added
-  function setRepeat() {
+//repeat button need repeat button added
+function setRepeat() {
     repeat = !repeat;
     //using font awesome instead of image
     imageName = repeat ? "green" : "red";
     document.querySelector("i.fa-repeat").style.color = imageName;
-  }
+}
 
-  //set mute button
-  function setMute() {
+//set mute button
+function setMute() {
     audioElement.audio.muted = !audioElement.audio.muted;
     var imageName = audioElement.audio.muted ? "green" : "red";
     document.querySelector("i.fa-volume-up").style.color = imageName;
-  }
+}
 
-  //set shuffle
-  function setShuffle() {
+//set shuffle
+function setShuffle() {
     shuffle = !shuffle;
     var imageName = shuffle ? "green" : "red";
     document.querySelector("i.fa-random").style.color = imageName;
 
     if (shuffle == true) {
-      //Randomize playlist
-      shuffleArray(shufflePlaylist);
-      currentIndex = shufflePlaylist.indexOf(audioElement.currentlyPlaying.id);
+        //Randomize playlist
+        shuffleArray(shufflePlaylist);
+        currentIndex = shufflePlaylist.indexOf(audioElement.currentlyPlaying.id);
     } else {
-      //shuffle has been deactivated
-      //go back to regular playlist
-      currentIndex = currentPlaylist.indexOf(audioElement.currentlyPlaying.id);
+        //shuffle has been deactivated
+        //go back to regular playlist
+        currentIndex = currentPlaylist.indexOf(audioElement.currentlyPlaying.id);
     }
-  }
+}
 
-  //shuffle array function from stackoverflow
-  function shuffleArray(a) {
+//shuffle array function from stackoverflow
+function shuffleArray(a) {
     var j, x, i;
     for (i = a.length; i; i--) {
-      j = Math.floor(Math.random() * i);
-      x = a[i - 1];
-      a[i - 1] = a[j];
-      a[j] = x;
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
     }
-  }
+}
 
-  //Set Audio tracks to to be played in tracklist
-  function setTrack(trackId, newPlaylist, play) {
+//Set Audio tracks to to be played in tracklist
+function setTrack(trackId, newPlaylist, play) {
 
     if (newPlaylist != currentPlaylist) {
-      currentPlaylist = newPlaylist;
-      //add shuffle
-      // shufflePlaylist = currentPlaylist.slice();
-      // shuffleArray(shufflePlaylist);
+        currentPlaylist = newPlaylist;
+        //add shuffle
+        // shufflePlaylist = currentPlaylist.slice();
+        // shuffleArray(shufflePlaylist);
     }
 
     //create tracklist index
@@ -178,69 +178,69 @@ $jsonArray = json_encode($array, JSON_UNESCAPED_SLASHES);
     formData.append("songId", trackId);
 
     fetch(url, {
-      method: 'POST',
-      body: formData
-
-    }).then(function(response) {
-
-      return response.text();
-
-    }).then(function(body) {
-
-      var track = JSON.parse(body);
-
-      if (track[0] != null) {
-        document.querySelector("div.audio_controls h1.trackName").textContent = track[0].songtitle;
-        audioElement.setTrack(track);
-
-      } else {
-        document.querySelector("div.audio_controls h1.trackName").textContent = "No Tracks Available";
-      }
-
-      if (play == true) {
-        playSong();
-      }
-
-    }).catch(function(err) {
-      console.error('Fetch Error :-S', err);
-    });
-
-  }
-
-  //Play song
-  function playSong() {
-
-    //track plays function needs ajax file updatePlays.php
-    if (audioElement.audio.currentTime == 0) {
-      //get tracks from database
-      let url = 'updatePlays.php';
-      let formData = new FormData();
-      formData.append("songId", audioElement.currentlyPlaying.id);
-
-      fetch(url, {
         method: 'POST',
         body: formData
 
-      }).then(function(response) {
+    }).then(function(response) {
 
         return response.text();
 
-      }).catch(function(err) {
+    }).then(function(body) {
+
+        var track = JSON.parse(body);
+
+        if (track[0] != null) {
+            document.querySelector("div.audio_controls h1.trackName").textContent = track[0].songtitle;
+            audioElement.setTrack(track);
+
+        } else {
+            document.querySelector("div.audio_controls h1.trackName").textContent = "No Tracks Available";
+        }
+
+        if (play == true) {
+            playSong();
+        }
+
+    }).catch(function(err) {
         console.error('Fetch Error :-S', err);
-      });
+    });
+
+}
+
+//Play song
+function playSong() {
+
+    //track plays function needs ajax file updatePlays.php
+    if (audioElement.audio.currentTime == 0) {
+        //get tracks from database
+        let url = 'updatePlays.php';
+        let formData = new FormData();
+        formData.append("songId", audioElement.currentlyPlaying.id);
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+
+        }).then(function(response) {
+
+            return response.text();
+
+        }).catch(function(err) {
+            console.error('Fetch Error :-S', err);
+        });
 
     }
     $(".player-button.play").hide();
     $(".player-button.pause").show();
     audioElement.play();
-  }
+}
 
-  //Pause Song
-  function pauseSong() {
+//Pause Song
+function pauseSong() {
     $(".player-button.play").show();
     $(".player-button.pause").hide();
     audioElement.pause();
-  }
+}
 </script>
 
 <section id="main_text" class="group">
@@ -251,113 +251,107 @@ $jsonArray = json_encode($array, JSON_UNESCAPED_SLASHES);
 
     <div class="product-box-large">
 
-      <h1><?= $singleartist->artist_name; ?></h1>
+          <h1><?= $singleartist->artist_name; ?></h1>
+          <!-- <button id="artist_btn" onclick="firstSong()">Play</button> -->
+          <!--Audio Controls-->
+          <div class="audio_controls">
 
-      <!-- <button id="artist_btn" onclick="firstSong()">Play</button> -->
-      <!--Audio Controls-->
-      <div class="audio_controls">
+            <h1 class="trackName"></h1>
 
-        <h1 class="trackName"></h1>
+            <div class="audiocntrl_containers">
 
-        <div class="audiocntrl_containers">
+                <div role="button" tabindex="0" class="player-button shuffle" onclick="setShuffle()">
+                    <i class="fa fa-random" aria-hidden="true" title="shuffle"></i>
+                </div>
 
-          <div role="button" tabindex="0" class="player-button shuffle" onclick="setShuffle()">
-            <i class="fa fa-random" aria-hidden="true" title="shuffle"></i>
-          </div>
+                <div role="button" tabindex="0" class="player-button play" onclick="playSong()">
+                    <i class="fa fa-play" aria-hidden="true" title="play"></i>
+                </div>
 
-          <div role="button" tabindex="0" class="player-button play" onclick="playSong()">
-            <i class="fa fa-play" aria-hidden="true" title="play"></i>
-          </div>
+                <div role="button" tabindex="0" class="player-button pause" style="display: none;"
+                    onclick="pauseSong()">
+                    <i class="fa fa-pause" aria-hidden="true" title="pause"></i>
+                </div>
 
-          <div role="button" tabindex="0" class="player-button pause" style="display: none;" onclick="pauseSong()">
-            <i class="fa fa-pause" aria-hidden="true" title="pause"></i>
-          </div>
+                <div role="button" tabindex="0" class="player-button previous" onclick="prevSong()">
+                    <i class="fa fa-step-backward" aria-hidden="true" title="previous"></i>
+                </div>
 
-          <div role="button" tabindex="0" class="player-button previous" onclick="prevSong()">
-            <i class="fa fa-step-backward" aria-hidden="true" title="previous"></i>
-          </div>
+                <div role="button" tabindex="0" class="player-button next" onclick="nextSong()">
+                    <i class="fa fa-step-forward" aria-hidden="true" title="next"></i>
+                </div>
 
-          <div role="button" tabindex="0" class="player-button next" onclick="nextSong()">
-            <i class="fa fa-step-forward" aria-hidden="true" title="next"></i>
-          </div>
-
-          <div role="button" tabindex="0" class="player-button repeat" onclick="setRepeat()">
-            <i class="fa fa-repeat" aria-hidden="true" title="repeat"></i>
-          </div>
+                <div role="button" tabindex="0" class="player-button repeat" onclick="setRepeat()">
+                    <i class="fa fa-repeat" aria-hidden="true" title="repeat"></i>
+                </div>
 
 
 
-          <!--add onclick="setMute() to change volume icon. need to add volume icon-->
-          <div class="audio_volume">
-            <div class="VolumeBg">
-              <div class="volume"></div>
-              <!--<input type="range" class="volume" title="volume" min="0" max="1" step="0.1" value="1">-->
+                <!--add onclick="setMute() to change volume icon. need to add volume icon-->
+                <div class="audio_volume">
+                    <div class="VolumeBg">
+                        <div class="volume"></div>
+                        <!--<input type="range" class="volume" title="volume" min="0" max="1" step="0.1" value="1">-->
+                    </div>
+                    <div class="VolumeImg" onclick="setMute()" role="button" tabindex="0">
+                        <i class="fa fa-volume-up" aria-hidden="true" title="mute"></i>
+                    </div>
+                </div>
             </div>
-            <div class="VolumeImg" onclick="setMute()" role="button" tabindex="0">
-              <i class="fa fa-volume-up" aria-hidden="true" title="mute"></i>
+            <div class="audiocntrl_containers">
+                <div class="current_time">00:00</div>
+                <div class="progress">
+                    <div class="play_progress"></div>
+                </div>
+                <div class="duration">00:00</div>
             </div>
+
           </div>
-        </div>
-        <div class="audiocntrl_containers">
-          <div class="current_time">00:00</div>
-          <div class="progress">
-            <div class="play_progress"></div>
-          </div>
-          <div class="duration">00:00</div>
-        </div>
-
-      </div>
-      <h2>Artist Tracks </h2>
-      <br />
-      <br />
-      <!--Audio Playlist-->
-      <ul class="audio-tracklist">
-        <?php
-
-        $i = 1;
-
-        foreach ($singleaudio as $songId) :
-
-          echo "<li>
-                  <span class=\"tracknum\">Track " . $i . " : </span>
-                  <span class=\"trackname\">" . $songId[1] . "</span>
-                  <span class=\"trackbtn\" onclick='setTrack(\"" . $songId[0] . "\", tempPlaylist, true)'><i class=\"fa fa-play\" aria-hidden=\"true\"></i> </span>
-                  <span class=\"trackplays\">plays</span>
-                </li>";
-          $i = $i + 1;
-        endforeach;
-
-        ?>
-      </ul>
-
-      <!--Temporary Play List-->
-      <script>
-        //songId value from value of $singleaudio variable
-        var tempSongIds = '<?= json_encode($songId[0]); ?>';
-        tempPlaylist = JSON.parse(tempSongIds);
-      </script>
+          <h2>Artist Tracks </h2>
+          <br />
+          <br />
+          <!--Audio Playlist-->
+          <ul class="audio-tracklist">
+            <?php
+              $i = 1;
+              foreach ($singleaudio as $songId) :
+                echo "<li>
+                        <span class=\"tracknum\">Track " . $i . " : </span>
+                        <span class=\"trackname\">" . $songId[1] . "</span>
+                        <span class=\"trackbtn\" onclick='setTrack(\"" . $songId[0] . "\", tempPlaylist, true)'><i class=\"fa fa-play\" aria-hidden=\"true\"></i> </span>
+                        <span class=\"trackplays\">plays</span>
+                      </li>";
+                $i = $i + 1;
+              endforeach;
+            ?>
+          </ul>
+          <!--Temporary Play List-->
+          <script>
+            //songId value from value of $singleaudio variable
+            var tempSongIds = '<?= json_encode($songId[0]); ?>';
+            tempPlaylist = JSON.parse(tempSongIds);
+          </script>
 
     </div>
 
     <h2> Other Albums</h2>
 
     <?php foreach ($singlealbums as $value) : ?>
-      <div class="product-box">
-        <figure class="product-info">
-          <a href="/singleresult?artistid=<?= $value->artistId ?>&albumid=<?= $value->id ?>">
-            <img src="/assets/databasepics/WEBP/<?= $value->image ?>" alt="Album-Cover-Image" />
-          </a>
-          <figcaption>
-            <ul class="product-box-info">
-              <li><?= $value->album ?></li>
-              <li><?= $value->genre ?></li>
-            </ul>
-          </figcaption>
-        </figure>
-      </div>
+    <div class="product-box">
+      <figure class="product-info">
+        <a href="/singleresult?artistid=<?= $value->artistId ?>&albumid=<?= $value->id ?>">
+          <img src="/assets/databasepics/WEBP/<?= $value->image ?>" alt="Album-Cover-Image" />
+        </a>
+        <figcaption>
+          <ul class="product-box-info">
+            <li><?= $value->album ?></li>
+            <li><?= $value->genre ?></li>
+          </ul>
+        </figcaption>
+      </figure>
+    </div>
     <?php endforeach; ?>
 
-    <div>
-      <div>
-        <br />
+  </div>
+  <br />
 </section>
